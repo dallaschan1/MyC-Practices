6aea2cce6998cea700e4ec0e3185a2f5a48deb24
Dallas <S10258596@connect.np.edu.sg>
2023-11-21 00:35:35 +0800

    Prac 6

---
+++ 6th prac(manipulating arrays and dict)/prac6/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] num = {4, 2, 2, 8, 5, 4, 8, 1};
+        SortedList<int, int> Numbers = new SortedList<int, int>();
+        foreach (int nu in num)
+        {
+            if (Numbers.ContainsKey(nu) == false)
+            {
+                Numbers.Add(nu, 1);
+            }
+            else
+            {
+                Numbers[nu] += 1;
+            }
+        }
+
+        foreach (var kvp in Numbers)
+        {
+            Console.WriteLine($"Number: {kvp.Key} Occurances: {kvp.Value}");
+        }
+
+    }
+}
+
