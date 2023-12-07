b43025eb4328b6080a2ac893686081c8061ecb19
dallaschan1 <s10258596@connect.np.edu.sg>
2023-12-07 15:22:07 +0800

    Common test prac

---
+++ 7th prac/PhoneApp_S10258596G/PhoneApp_S10258596G/Program.cs
@@ -0,0 +1,148 @@
+﻿class Program
+{ 
+    private static List<Phone> phonelist = new List<Phone>();
+
+    public static void Main(String[] args)
+    {
+        InitialisePhones();
+        while (true)
+        {
+            DisplayOutput();
+            UpdateUsage();
+        }
+
+    }
+
+    public static void InitialisePhones()
+    {
+        string file = "PhoneDetails.csv";
+        using (StreamReader sr = new StreamReader(file)) 
+        {
+            string s = sr.ReadLine();
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                string PhoneNum = s.Split(',')[0];
+                int usage = Int32.Parse(s.Split(',')[1]);
+                string PlanType = s.Split(",")[2];
+
+                Phone phonesdetail = new Phone(PhoneNum, usage, PlanType);
+                phonelist.Add(phonesdetail);
+            }
+        }
+    
+    }
+
+    public static void DisplayOutput()
+    {
+        Console.WriteLine("PhoneNo   Usage   PlanType   PhoneCharge($)");
+        foreach (Phone phone in phonelist) 
+        {
+            Console.WriteLine($"{phone.PhoneNum,-10}{phone.Usage,-8}{phone.PlanType,-11}{phone.CalculateCharge(phone.Usage, phone.PlanType),13:N2}");
+
+        }
+    }
+
+    public static void UpdateUsage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Update Phone Usage");
+        Console.WriteLine("------------------");
+        Console.Write("Enter phone no: ");
+        string number = Console.ReadLine();
+        foreach (Phone phones in phonelist)
+        {
+            if (number == phones.PhoneNum)
+            {
+                Console.WriteLine($"Current usage: {phones.Usage}");
+                Console.Write("Enter new Usage: ");
+                int use = int.Parse(Console.ReadLine());
+                phones.Usage = use;
+                Console.WriteLine("The new usage is updated successfully");
+                Console.WriteLine();
+                DisplayOutput();
+                Console.WriteLine();
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Phone not found!");
+                Console.WriteLine();
+                break;
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+class Phone
+{
+    private string phoneNum;
+    private int usage;
+    private string planType;
+
+    public string PhoneNum
+    {
+        get { return phoneNum; }
+        set { phoneNum = value; }
+    }
+
+    public int Usage
+    {
+        get { return usage; }
+        set { usage = value; }
+    }
+
+    public string PlanType
+    {
+        get { return planType; }
+        set { planType = value; }
+    }
+
+    public double CalculateCharge(int Use, string Plan)
+    {
+        if (Plan == "A")
+        {
+            return 0.5 * Use / 100;
+        }
+        else if (Plan == "B")
+        {
+            Use -= 1000;
+            if (Use > 0)
+            {
+                return Use * 0.2 / 100;
+            }
+        }
+
+        else
+        {
+            Use -= 5000;
+            if (Use > 0)
+            {
+                return Use * 0.1 / 100;
+            }
+        }
+
+        return 0;
+
+    }
+
+    public Phone(string PhoneNum, int Usage, string PlanType)
+    {
+        this.PhoneNum = PhoneNum;
+        this.Usage = Usage;
+        this.PlanType = PlanType;
+    }
+
+    public override string ToString()
+    {
+        return null;
+    }
+}
+
---
+++ Practice9/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the length (a) Square Pyramid in metre: ");
+        double length = double.Parse(Console.ReadLine());
+        Console.Write("Enter the height (h) of Square Pyramid in metre: ");
+        double height = Double.Parse(Console.ReadLine());
+        SquarePyramid squarePyramid = new SquarePyramid(length, height);
+        Console.WriteLine($"Volume of Square Pyramid : {squarePyramid.GetVolume()} cubic metre.");
+        Console.WriteLine($"Surface Area of Square Pyramid : {squarePyramid.GetSurfaceArea()} square metre.");
+
+
+    }
+}
+
+class SquarePyramid
+{
+    private double a;
+    private double b;
+
+    public double A { get { return a; } set { a = value; } }
+    public double B { get { return b; } set { b = value; } }
+
+    public SquarePyramid() { }
+    public SquarePyramid(double a, double b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double GetVolume()
+    {
+        return ((Math.Pow(A,2) * B) / 3);
+    }
+
+    public double GetSurfaceArea()
+    {
+        return (A * (A + Math.Sqrt(Math.Pow(A,2) + 4 * Math.Pow(B,2))));
+    }
+}
